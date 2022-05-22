b514ae226109c137af4a40907c617619a675ad9e
hfmarin <49383296+hfmarin@users.noreply.github.com>
2022-05-22 14:18:47 -0300

    Remove searchfield already done in SearchAppBar

--- app/src/main/java/com/hfmarin/meliapp/presentation/components/SearchField.kt
+++
@@ -1,8 +0,0 @@
-package com.hfmarin.meliapp.presentation.components
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun searchField(query: String, onValueChange: (string: String) -> Unit) {
-
-}
