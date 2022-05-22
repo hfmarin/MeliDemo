936dbaf380c22851a52877602e73426f4de10f79
hfmarin <49383296+hfmarin@users.noreply.github.com>
2022-05-22 14:18:03 -0300

    Remove not used dto

--- app/src/main/java/com/hfmarin/meliapp/network/model/Sort.kt
+++
@@ -1,6 +0,0 @@
-package com.hfmarin.meliapp.network.model
-
-data class Sort(
-    val id: String,
-    val name: String
-)
--- app/src/main/java/com/hfmarin/meliapp/network/model/State.kt
+++
@@ -1,6 +0,0 @@
-package com.hfmarin.meliapp.network.model
-
-data class State(
-    val id: String,
-    val name: String
-)
